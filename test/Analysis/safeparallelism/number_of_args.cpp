70ef139f874677022a71b18cf76f15dd4c9d180e
Alexandros Tzannes <atzannes@gmail.com>
2013-01-10 15:43:08 -0600

    added C++11 style attributes to number_of_args.cpp test

--- test/Analysis/safeparallelism/number_of_args.cpp
+++ test/Analysis/safeparallelism/number_of_args.cpp
@@ -1,6 +1,79 @@
-// RUN: %clang_cc1 -analyze -analyzer-checker=alpha.SafeParallelismChecker %s -verify
-//
+// RUN: %clang_cc1 -DASAP_GNU_SYNTAX -analyze -analyzer-checker=alpha.SafeParallelismChecker %s -verify
+// RUN: %clang_cc1 -DASAP_CXX11_SYNTAX -std=c++11 -analyze -analyzer-checker=alpha.SafeParallelismChecker %s -verify
 
+#ifdef ASAP_CXX11_SYNTAX
+// Correct number of arg annotations for the annotated types
+class 
+  [[asap::param("P")]]
+  [[asap::region("R")]]
+  [[asap::region("L")]]
+  [[asap::region("Links")]]
+    C0 {
+
+  int data [[asap::arg("P")]];
+
+  C0 *left [[asap::arg("Links"), asap::arg("P:L")]];
+
+  C0 *right[[asap::arg("Links"), asap::arg("P:R")]] ;
+
+  C0 **last_visited_link[[asap::arg("Links"),
+                          asap::arg("P"),
+                          asap::arg("P:*")]];
+
+  int *last_visited_data[[asap::arg("P"), asap::arg("P:*")]];
+
+};
+
+// Too many arg annotations
+class 
+  [[asap::param("P")]]
+  [[asap::region("R")]]
+  [[asap::region("L")]]
+  [[asap::region("Links")]]
+    C1 {
+
+  int data [[asap::arg("P:R")]]
+           [[asap::arg("P")]]    // expected-warning{{superfluous region argument}} 
+           [[asap::arg("P:L")]];   // expected-warning{{superfluous region argument}}
+
+  C1 *left[[asap::arg("P:L")]]   
+      [[asap::arg("Links")]]
+      [[asap::arg("Links")]]; // expected-warning{{superfluous region argument}} 
+
+  C1 *right  [[asap::arg("Links")]]
+             [[asap::arg("P:R")]]
+             [[asap::arg("Links")]];   // expected-warning{{superfluous region argument}}
+
+  C1 **last_visited_link  [[asap::arg("Links")]] 
+                          [[asap::arg("P")]] 
+                          [[asap::arg("P:*")]]   
+                          [[asap::arg("P")]]; // expected-warning{{superfluous region argument}} 
+
+  int *last_visited_data [[asap::arg("P")]]
+                         [[asap::arg("P")]]
+                         [[asap::arg("P:*")]]; // expected-warning{{superfluous region argument}};
+
+};
+
+// Too few arg annotations (the rest of them will use the defaults
+// or will be inferred)
+class 
+  [[asap::param("P")]]
+  [[asap::region("R")]]
+  [[asap::region("L")]]
+  [[asap::region("Links")]]
+    C2 {
+
+  int data;
+  C2 *left [[asap::arg("P:L")]];
+  C2 *right [[asap::arg("P:R")]];
+  C2 **last_visited_link [[asap::arg("P:*")]];
+  int *last_visited_data [[asap::arg("P:*")]];
+
+};
+#endif
+
+#ifdef ASAP_GNU_SYNTAX
 // Correct number of arg annotations for the annotated types
 class 
   __attribute__((param("P")))
@@ -73,3 +146,5 @@ class
   int __attribute__((arg("P:*"))) *last_visited_data;
 
 };
+
+#endif
