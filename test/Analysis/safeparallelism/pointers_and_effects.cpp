a5c6bc9535e708aa95576c6e955ea67354c1d24c
Alexandros Tzannes <atzannes@gmail.com>
2013-01-10 15:16:06 -0600

    adding version of tests using C++11 attributes

--- test/Analysis/safeparallelism/pointers_and_effects.cpp
+++ test/Analysis/safeparallelism/pointers_and_effects.cpp
@@ -3,6 +3,104 @@
 //
 
 
+#ifdef ASAP_CXX11_SYNTAX
+namespace Shapes /* FIXME: is this not in the C++11 standard ? [[asap::region("Pool")]]*/ 
+{
+
+class
+[[asap::region("X")]]
+[[asap::region("Y")]]
+[[asap::param("Pp")]]
+Point {
+public:
+  int x [[asap::arg("Pp:X")]];
+  int y [[asap::arg("Pp:Y")]];
+}; // end Point
+
+class
+//[[asap::region_param("P, P<=Pool")]]
+[[asap::param("Pr")]]
+[[asap::region("R1"), asap::region("R2"), 
+  asap::region("Next"), asap::region("Links"), asap::region("Pool")]]
+Rectangle {
+
+// Fields
+Point p1 [[asap::arg("Pr:R1")]];
+Point p2 [[asap::arg("Pr:R2")]];
+
+Point 
+  * pp [[asap::arg("Pr:Links"), asap::arg("Pool:*")]],
+  **ppp [[asap::arg("Links"), asap::arg("Pr:Links"), asap::arg("Pool:*")]];
+
+Point
+  * ppstar [[asap::arg("Pr:Links"), asap::arg("Pr:*")]],
+  * ppstar1 [[asap::arg("Pr:Links"), asap::arg("Pr:*:R1")]],
+  **pppstar [[asap::arg("Links"), asap::arg("Pr:*:Links"), asap::arg("Pr:*")]];
+
+Rectangle
+  * loop [[asap::arg("Pr:Links"), asap::arg("*")]],
+  * next [[asap::arg("Pr:Links"), asap::arg("Pr:Next")]],
+  ** pnext [[asap::arg("Links"), asap::arg("Pr:Links"), asap::arg("Pr:Next")]];
+
+Rectangle ** pnextstar [[asap::arg("Links"), 
+                         asap::arg("Pr:Links"), 
+                         asap::arg("Pr:*:Next")]];
+
+void do_pointer_stuff(int _x, int _y) 
+  [[asap::writes("Pr:R1:*")]]
+  [[asap::writes("Pr:R2:*")]]
+  //[[asap::writes("P:R1:Y")]] // Y not declared here TODO: support it
+  [[asap::writes("Pr:*:Links")]]
+  [[asap::writes("Links")]]
+  [[asap::reads("Pr:Next")]]
+{
+  p1.x = _x;          // writes Pr:R1:X
+  p1.y = _y;          // writes Pr:R1:Y
+  p2.x = _x + 5;      // writes Pr:R2:X
+  p2.y = _y + 5;      // writes Pr:R2:Y
+  p1.x = next->p1.x;
+  pp = &*&p1;         // writes Pr:Links                                                              expected-warning{{invalid assignment}}
+  ppstar = &*&p1;     // writes Pr:Links
+  ppp = &(*&pp);      // writes Links
+  *ppp = pp = &p2;         // reads Links, writes Pr:Links              expected-warning{{RHS region 'Pr:R2' is not included in LHS region 'Pool:*' invalid assignment}}
+  *ppp = ppstar = &p2;         // reads Links, writes Pr:Links          expected-warning{{RHS region 'Pr:*' is not included in LHS region 'Pool:*' invalid assignment}}
+  ppstar = &p2;         // reads Links, writes Pr:Links
+  ppstar1 = &p1;
+  ppstar1 = &p2;      // expected-warning{{invalid assignment}}
+  pppstar = &ppstar1;
+
+  *pppstar = &p2;     // reads Links, writes Pr:Links                   expected-warning{{cannot modify aliasing through pointer to partly specified region}}
+
+  *ppp = *&pp;        // reads Links, writes Pr:Links + reads Pr:Links
+  *ppp = next->pp;    // reads Links, writes Pr:Links + reads Pr:Links, Pr:Next:Links
+  *ppp = *&(next->pp);    // reads Links, writes Pr:Links + reads Pr:Links, Pr:Next:Links
+  ppp = &next->pp;        // writes Links + reads Pr:Links                                            expected-warning{{invalid assignment}}
+  pppstar = &next->ppstar;        // writes Links + reads Pr:Links
+  *ppp = *(*&next->ppp);  // reads Links, writes Pr:Links + reads Links, Pr:Links, Pr:Next:Links
+  *ppp = &*pp;            // reads Links, writes Pr:Links + reads Pr:Links
+  next = this;        // writes Pr:Links (the 'this' pointer is immutable, so   expected-warning{{RHS region 'Pr' is not included in LHS region 'Pr:Next' invalid assignment}}
+                      // write effects are not possible and read effects on it 
+                      // can be discarded
+  loop = this;
+  loop = next;
+
+  pnext = &next;              // writes Links 
+  *pnext = next->next;        // reads Links, writes Pr:Links + reads Pr:Links, Pr:Next:Links         expected-warning{{invalid assignment}}
+  *pnext = *next->pnext;      // reads Links, writes Pr:Links + reads Pr:Links, Links, Pr:Next:Links  expected-warning{{invalid assignment}}
+  *pnext = *(*pnext)->pnext;  // reads Links, writes Pr:Links + reads Links, Pr:Links, Pr:Next:Links  expected-warning{{invalid assignment}}
+  *pnextstar = next->next;        // reads Links, writes Pr:Links + reads Pr:Links, Pr:Next:Links
+  *pnextstar = *next->pnext;      // reads Links, writes Pr:Links + reads Pr:Links, Links, Pr:Next:Links
+  *pnextstar = *(*pnext)->pnext;  // reads Links, writes Pr:Links + reads Links, Pr:Links, Pr:Next:Links
+}
+
+// TODO 
+void do_reference_stuff() ;
+
+}; // end class Rectangle
+
+} // end namespace Pool
+#endif
+
 #ifdef ASAP_GNU_SYNTAX
 namespace Shapes __attribute__((region("Pool"))) {
 
@@ -99,7 +197,4 @@ void do_reference_stuff() ;
 } // end namespace Pool
 #endif
 
-#ifdef ASAP_CXX11_SYNTAX
-// expected-no-diagnostics
 
-#endif
--- test/Analysis/safeparallelism/regions.cpp
+++ test/Analysis/safeparallelism/regions.cpp
@@ -1,7 +1,103 @@
-// RUN: %clang_cc1 -analyze -analyzer-checker=alpha.SafeParallelismChecker %s -verify
-//
+// RUN: %clang_cc1 -DASAP_CXX11_SYNTAX -std=c++11 -analyze -analyzer-checker=alpha.SafeParallelismChecker %s -verify
+// RUN: %clang_cc1 -DASAP_GNU_SYNTAX -analyze -analyzer-checker=alpha.SafeParallelismChecker %s -verify
+// 
 
+#ifdef ASAP_CXX11_SYNTAX
+/// Test Valid Region Names & Declared RPL elements
+class 
+[[asap::region("1R1")]]        //expected-warning {{invalid region name}}
+[[asap::param("1P1")]]  // expected-warning {{invalid region parameter name}} 
+[[asap::region("R1"), asap::region("R2"), asap::region("R3"), 
+               asap::region("_R2"), asap::region("R_2"), asap::region("R_2_c")]]
+C0 { 
+};
 
+class
+[[asap::region("R2")]]
+[[asap::region("R3")]]
+[[asap::param("P1")]]
+C1 { 
+  // TODO: check that money is not annotated with an arg annotation
+  int money0 [[asap::arg("P1:R1")]]; // expected-warning {{RPL element was not declared}} 
+  int money [[asap::arg("P1:R3")]]; 
+  C1 *next [[asap::arg("P1"), asap::arg("P1:R2")]];
+
+public:
+  [[asap::param("*")]] // expected-warning {{invalid region parameter name}}
+  C1 (): money(70) {} 
+
+  [[asap::param("Pc")]]
+  C1 (C1& [[asap::arg("Pc")]] c)
+    [[asap::reads("Pc:*")]]
+    [[asap::writes("P1:*")]]
+    :
+    money(c.money) ,
+    next(c.next)
+  {}
+
+  int get_money() [[asap::reads("P1:R3")]] 
+  { return money; }
+
+  int get_next_money() [[asap::reads("P1")]] [[asap::reads("P1:R2:R3")]]
+  { return next->money; }
+
+  void set_money(int cash) 
+    [[asap::writes("P1:R3")]]
+  { money = cash; }
+
+  int close_account() [[asap::reads("P1:R3")]] {
+    int balance = money;
+    set_money(0);
+    return balance;
+  }
+
+  void set_next_money(int cash) 
+    [[asap::reads("P1")]]
+    [[asap::writes("P1:R2:R3")]]
+  { next->money = cash; }
+
+  void set_next_money_to_this_money () 
+    [[asap::writes("P1:R2:R3")]]
+    [[asap::reads("P1")]]
+    [[asap::reads("P1:R3")]]
+  { next->money = money; }
+
+  void do_stuff(int cash) 
+    [[asap::reads("P1")]]
+    [[asap::reads("P1:R2")]]
+    [[asap::reads("P1:R2:R3")]]
+    [[asap::reads("P1:R2:R2:R3")]] 
+    [[asap::reads("P1:R3")]]         // expected-warning{{effect summary is not minimal}}
+    [[asap::atomic_reads("P1:R3")]]  // expected-warning{{effect summary is not minimal}}
+    [[asap::atomic_writes("P1:R3")]] // expected-warning{{effect summary is not minimal}}
+    [[asap::writes("P1:R3")]] 
+    [[asap::no_effect]]                   // expected-warning{{effect summary is not minimal}}
+  { 
+    cash += next->money;        // reads P1, P1:R2:R3
+    cash -= next->next->money;  // reads P1, P1:R2, P1:R2:R2:R3
+    money++;                    // writes P1:R3
+    money--;                    // writes P1:R3
+    ++money;                    // writes P1:R3
+    --money;                    // writes P1:R3
+    money = cash + 4 + cash;    // writes P1:R3
+    !cash ? (cash = money) : money++;   // writes P1:R3 or reads P1:R3
+    cash ? money++ : (cash = money = 42);   // writes P1:R3 or reads P1:R3
+  }
+  [[asap::region("R3")]]
+  void add_money(int cash) {
+    money += cash; // expected-warning {{effect not covered by effect summary}}
+  }
+  void subtract_money(int cash) [[asap::reads("P1:R1")]] { // expected-warning {{RPL element was not declared}}  
+    money -= (cash) ? cash+3 : cash=3;  // expected-warning{{effect not covered by effect summary}}
+    money -= cash;                      // expected-warning{{effect not covered by effect summary}}
+  }
+
+  bool insured [[asap::arg("P1:R1")]];// expected-warning {{RPL element was not declared}} 
+  char* name [[asap::arg("P1")]];
+};
+#endif
+
+#ifdef ASAP_GNU_SYNTAX
 /// Test Valid Region Names & Declared RPL elements
 class 
 __attribute__((region("1R1")))        //expected-warning {{invalid region name}}
@@ -43,6 +139,12 @@ public:
   void set_money(int cash) 
     __attribute__((writes("P1:R3")))
   { money = cash; }
+
+  int close_account() __attribute__((reads("P1:R3"))) {
+    int balance = money;
+    set_money(0);
+    return balance;
+  }
 
   void set_next_money(int cash) 
     __attribute__((reads("P1")))
@@ -88,4 +190,5 @@ public:
   bool insured __attribute__((arg("P1:R1")));// expected-warning {{RPL element was not declared}} 
   char* name __attribute__((arg("P1")));
 };
+#endif
 
