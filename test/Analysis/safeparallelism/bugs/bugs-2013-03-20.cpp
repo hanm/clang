119d92415654144490944fc8b305b7493b78901d
Alexandros Tzannes <atzannes@gmail.com>
2013-03-20 22:05:53 -0500

    Added file to test bug which was just fixed by last commit.

---
+++ test/Analysis/safeparallelism/bugs/bugs-2013-03-20.cpp
@@ -0,0 +1,27 @@
+// RUN: %clang_cc1 -std=c++11 -analyze -analyzer-checker=alpha.SafeParallelismChecker %s -verify
+// expected-no-diagnostics
+
+class [[asap::param("P")]] point  {
+  double m_x [[asap::arg("P")]];
+  double m_y [[asap::arg("P")]];
+
+public:
+  point() {}
+  point(double x, double y) : m_x(x), m_y(y) {}
+  point(const point &p)
+      : m_x(p.m_x), m_y(p.m_y)
+      {}
+
+  point &operator= [[asap::arg("P"), asap::param("Q"), asap::reads("Q"), asap::writes("P")]] (
+    const point &p [[asap::arg("Q")]]
+    )
+    {
+    m_x = p.m_x;
+    m_y = p.m_y;
+
+    // Using 'tmp' is a work-around for an ASP bug
+    // point *tmp [[asap::arg("class")]] = this;
+    //return *tmp;
+    return *this;
+    }
+};
