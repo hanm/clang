6b01492f320d94c04a3333c6e3d760a943c80902
Alexandros Tzannes <atzannes@gmail.com>
2013-05-28 11:28:01 -0400

    Small fix after merge

--- lib/StaticAnalyzer/Checkers/Rpl.h
+++ lib/StaticAnalyzer/Checkers/Rpl.h
@@ -26,20 +26,6 @@
 namespace clang {
 namespace asap {
 
-//#define ASAP_DEBUG
-//#define ASAP_DEBUG_VERBOSE2
-#ifdef ASAP_DEBUG
-  static raw_ostream& os = llvm::errs();
-#else
-  static raw_ostream &os = llvm::nulls();
-#endif
-
-#ifdef ASAP_DEBUG_VERBOSE2
-  static raw_ostream &OSv2 = llvm::errs();
-#else
-  static raw_ostream &OSv2 = llvm::nulls();
-#endif
-
 class RplElement {
 public:
   /// Type
