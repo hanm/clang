ada28ce3acf65ecc01be5ef2238572deaefba544
Alexandros Tzannes <atzannes@gmail.com>
2014-03-24 13:04:27 -0400

    Fixing parametric point example (and removing quasi-identical test)

--- test/Analysis/safeparallelism/inference/effects/prolog/point_param.cpp
+++ test/Analysis/safeparallelism/inference/effects/prolog/point_param.cpp
@@ -5,13 +5,17 @@
 class [[asap::param("P"), asap::region("Rx,Ry")]] Point {
 	int x [[asap::arg("P:Rx")]];
 	int y [[asap::arg("P:Ry")]];
- public:
+    [[asap::param("Q")]] Point (const Point &P[[asap::arg("Q")]]);
+public:
+    Point() : x(0), y(0) {}
+    Point(int x_, int y_) : x(x_), y(y_) {}
+    
 	void setX(int _x) { x = _x; }
 	void setY(int _y) { y = _y; }
-	void setXY(int _x, int _y);
+	void setXY(int _x, int _y) { setX(_x); setY(_y); }
 };
 
-void foo() {
+void foo [[asap::region("Rfoo")]] () {
 	Point *p1 [[asap::arg("Rfoo")]] = new Point();
 	p1->setX(4);
 }
