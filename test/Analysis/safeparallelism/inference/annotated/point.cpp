8a0e3c050dd8a59c2f0197b56cdf821521538479
Michael Han <fragmentshaders@gmail.com>
2014-04-07 21:33:41 -0700

    Update point sample with rpl sample.

--- test/Analysis/safeparallelism/inference/annotated/point.cpp
+++ test/Analysis/safeparallelism/inference/annotated/point.cpp
@@ -19,6 +19,26 @@ public:
     void setX [[asap::writes("P")]](double x_) { *getXPtr() = x_; }
     }; // end class point
 
+class [[asap::param("P"), asap::region("R1, R2")]] point_rpl  {
+  double x [[asap::arg("P:R1")]];
+  double y [[asap::arg("P:R2")]];
+public:
+  point_rpl() {}
+  point_rpl(double x_, double y_) : x(x_), y(y_) {}
+  // implicitly: [[asap::reads("Local")]] point(double x_ [[asap:arg("Local")]], double y_ [[asap::arg("Local")]]) : x(x_), y(y_) {}
+  
+  // some functions added for testing checker features & corner cases
+  
+  double *getXPtr [[asap::arg("Local,P:R1")]] () { return &x; }
+  double getX [[asap::reads("P:R1")]] () { return x; }
+  double &getXRef [[asap::arg("P:R1")]] () { return x; }
+  
+  void setX1(double x_) { *getXPtr() = x_; } // expected-warning{{effect not covered by effect summary}}
+  void setX2[[asap::reads("P:R1")]](double x_) { *getXPtr() = x_; } // expected-warning{{effect not covered by effect summary}}
+  void setX3[[asap::reads("P:R1")]](double x_) { getXRef() = x_; } // expected-warning{{effect not covered by effect summary}}
+  void setX [[asap::writes("P:R1")]](double x_) { *getXPtr() = x_; }
+};
+
 class [[asap::param("P"), asap::region("R")]]  write_functor {
 
 public:
