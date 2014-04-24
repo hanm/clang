e968bda776d916cbebc4449f3db21f48838549f9
Alexandros Tzannes <atzannes@gmail.com>
2014-04-23 22:12:48 -0500

    Modified point.cpp test to match the new expected output.

--- test/Analysis/safeparallelism/inference/effects/point.cpp
+++ test/Analysis/safeparallelism/inference/effects/point.cpp
@@ -6,7 +6,10 @@ class [[asap::region("Rx,Ry")]] Point {
   int x [[asap::arg("Rx")]];
   int y [[asap::arg("Ry")]];
  public:
-  void setX(int _x) { x = _x; } // expected-warning{{Solution for setX: [Reads Effect on Local,Writes Effect on Rx]}}
-  void setY(int _y) { y = _y; } // expected-warning{{Solution for setY: [Reads Effect on Local,Writes Effect on Ry]}}
-  void setXY(int _x, int _y);
+  void setX(int _x) { x = _x; } // expected-warning{{Inferred Effect Summary for setX: [reads(rpl([rLOCAL],[])),writes(rpl([r0_Rx],[]))]}}
+  void setY(int _y) { y = _y; } // expected-warning{{Inferred Effect Summary for setY: [reads(rpl([rLOCAL],[])),writes(rpl([r1_Ry],[]))]}}
+  void setXY(int _x, int _y) {  // expected-warning{{Inferred Effect Summary for setXY: [writes(rpl([r0_Rx],[])),writes(rpl([r1_Ry],[])),reads(rpl([rLOCAL],[]))]}}
+    setX(_x);
+    setY(_y);
+  }
 };
