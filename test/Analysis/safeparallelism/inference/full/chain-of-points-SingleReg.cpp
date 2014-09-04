c07266f15e72eed6a0df18cd80c83ee3583b1349
Alexandros Tzannes <atzannes@gmail.com>
2014-09-03 21:52:27 -0500

    Updating expected result now that we added the Global region to the parent of all rpl_domains

--- test/Analysis/safeparallelism/inference/full/chain-of-points-SingleReg.cpp
+++ test/Analysis/safeparallelism/inference/full/chain-of-points-SingleReg.cpp
@@ -39,10 +39,10 @@ public:
 struct //[[asap::param("Pl")]] 
        link {
     point pos;  // expected-warning{{Inferred region arguments: class point, IN:<empty>, ArgV:[r3_pos]}}
-    link *next; // expected-warning{{Inferred region arguments: struct link *, IN:[r4_next], ArgV:[rSTAR]}}
+    link *next; // expected-warning{{Inferred region arguments: struct link *, IN:[r4_next], ArgV:[rGLOBAL]}}
 
     //[[asap::param("Q"), asap::reads("Q")]]
-    link(const point &pos_in /*[[asap::arg("Pl")]]*/) // expected-warning{{Inferred region arguments: const class point &, IN:<empty>, ArgV:[rSTAR]}}
+    link(const point &pos_in /*[[asap::arg("Pl")]]*/) // expected-warning{{Inferred region arguments: const class point &, IN:<empty>, ArgV:[rGLOBAL]}}
         :
         pos(pos_in),
         next(nullptr) {}
@@ -64,22 +64,22 @@ void delete_all[[asap::param("Q")]]//, asap::writes("Q")]]
  
 class //[[asap::param("P")]] 
     chain {
-    link *start;  // expected-warning{{Inferred region arguments: struct link *, IN:[r10_start], ArgV:[rSTAR]}}
+    link *start;  // expected-warning{{Inferred region arguments: struct link *, IN:[r10_start], ArgV:[rGLOBAL]}}
 public:
     chain() : start(nullptr) {}
     /*[[asap::writes("P")]]*/ ~chain() { delete_all(start); } //expected-warning{{Inferred Effect Summary for ~chain: [reads(rpl([r10_start],[])),reads(rpl([r4_next],[])),reads(rpl([rLOCAL],[]))]}}
    
     // Add a link to the start of the chain
-    void add_link /*[[asap::writes("P")]]*/ (const point &pos /*[[asap::arg("P")]]*/)  // expected-warning{{Inferred region arguments: const class point &, IN:<empty>, ArgV:[r12_pos]}}
+    void add_link /*[[asap::writes("P")]]*/ (const point &pos /*[[asap::arg("P")]]*/)  // expected-warning{{Inferred region arguments: const class point &, IN:<empty>, ArgV:[rGLOBAL]}}
         {//expected-warning{{Inferred Effect Summary for add_link: [reads(rpl([r10_start],[])),reads(rpl([r13_new_start],[])),writes(rpl([r10_start],[])),writes(rpl([r4_next],[]))]}}
-        link *new_start /*[[asap::arg("P, P")]]*/ = new link(pos);  // expected-warning{{Inferred region arguments: struct link *, IN:[r13_new_start], ArgV:[r14_new_start]}}
+        link *new_start /*[[asap::arg("P, P")]]*/ = new link(pos);  // expected-warning{{Inferred region arguments: struct link *, IN:[r13_new_start], ArgV:[rGLOBAL]}}
         new_start->next = start;
         start = new_start;
         }
     // count and return the number of nodes in the list
     int n_points /*[[asap::reads("P")]]*/ () const
         { // expected-warning{{Inferred Effect Summary for n_points: [reads(rpl([r10_start],[])),reads(rpl([r15_lp],[])),reads(rpl([r4_next],[])),writes(rpl([r15_lp],[])),writes(rpl([rLOCAL],[]))]}}
-        link *lp /*[[asap::arg("Local, P")]]*/ = start; // expected-warning{{Inferred region arguments: struct link *, IN:[r15_lp], ArgV:[rSTAR]}}
+        link *lp /*[[asap::arg("Local, P")]]*/ = start; // expected-warning{{Inferred region arguments: struct link *, IN:[r15_lp], ArgV:[rGLOBAL]}}
         int count /*[[asap::arg("Local")]]*/ = 0;
         while(lp)
             {
@@ -95,7 +95,7 @@ public:
     // If there are fewer than n links in the chain return false, and do
     // not change either chain.
     bool split /*[[asap::writes("P")]]*/ (int n, chain &rest /*[[asap::arg("P")]]*/) {//expected-warning{{Inferred Effect Summary for split: [writes(rpl([r10_start],[])),writes(rpl([r18_lp],[])),writes(rpl([r4_next],[])),writes(rpl([rLOCAL],[]))]}}  // expected-warning{{Inferred region arguments: class chain &, IN:<empty>, ArgV:[r17_rest]}}
-        link *lp/*[[asap::arg("Local, P")]]*/ = start;  // expected-warning{{Inferred region arguments: struct link *, IN:[r18_lp], ArgV:[rSTAR]}}
+        link *lp/*[[asap::arg("Local, P")]]*/ = start;  // expected-warning{{Inferred region arguments: struct link *, IN:[r18_lp], ArgV:[rGLOBAL]}}
         for(int i = 1; lp && i < n; ++i)
             {
             lp = lp->next;
