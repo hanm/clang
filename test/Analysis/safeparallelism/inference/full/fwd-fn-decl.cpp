9762b87c368b6f751872542463e570f0753d8f12
Alexandros Tzannes <atzannes@gmail.com>
2015-04-28 15:09:12 -0400

    Adding test to fix issue with inferred asap::arg when there is a forward declaration of the function (Currently making two copies of the RPL variable instead of one)

---
+++ test/Analysis/safeparallelism/inference/full/fwd-fn-decl.cpp
@@ -0,0 +1,12 @@
+// RUN: %clang_cc1 -std=c++11 -analyze -analyzer-checker=alpha.SafeParallelismChecker -analyzer-config -asap-default-scheme=inference %s -verify
+
+class [[asap::param("P")]] C {
+  int v [[asap::arg("P")]];
+public:
+  C &foo [[asap::param("P2")]] (C &x [[asap::arg("P2")]]);
+}; // end class C1
+
+C &C::foo /*param(P2) */ (C &x [[asap::arg("P2")]]) { // expected-warning{{Inferred region arguments: class C &(class C &), IN:<empty>, ArgV:[p0_P]: Inferred region arguments}} // expected-warning{{Inferred Effect Summary for foo: [reads(rpl([p1_P2],[])),writes(rpl([p0_P],[]))]:}}
+  v = v * x.v;
+  return *this;
+}
